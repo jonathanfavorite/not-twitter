9da7dc6977337d7e15f950a553641f7b0ef35ee3
Jonathan Favorite <jonathanfavorite@hotmail.com>
2022-01-25 18:35:15 -0500

    test

--- src/components/specific/Dashboard/Dashboard.js
+++ src/components/specific/Dashboard/Dashboard.js
@@ -6,8 +6,20 @@ import Tweet from '../../global/tweet/Tweet';
 import Loading from '../../global/Loading/Loading';
 import Timeline from '../../global/timeline/Timeline';
 export default function Dashboard() {
+
+    const [screen, setScreen] = useState(window.innerWidth);
+    
+    window.addEventListener('resize', () => {
+        setScreen(window.innerWidth);
+    });
+
+    useEffect(() => {
+
+    }, [screen])
+
     return (
         <>
+            <h1>screen size is: {screen}</h1>
             <Location locationDetails="" />
             <ComposeTweetWidget />
              
